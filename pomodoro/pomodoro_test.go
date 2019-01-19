8b15ee2f8b095841505e0984a52f648d0a134ac4
Mark Gustetic <mark.gustetic@gmail.com>
2019-01-18 19:53:28 -0500

    setup count test

--- pomodoro/pomodoro_test.go
+++ pomodoro/pomodoro_test.go
@@ -1,1 +1,12 @@
 package pomodoro
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestEmptyCount(t *testing.T) {
+	pomodoro := Pomodoro{}
+
+  assert.Equal(t, pomodoro.PomodoroCount(), 0)
+}
